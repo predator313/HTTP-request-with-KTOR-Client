baee5da26d6764d303643c2a02a3962265969d98
Aamir Ashraf <aamirnasa97@gmail.com>
2024-06-17 18:29:11 +0530

    all dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        // other repositories...
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath ("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
+    }
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
