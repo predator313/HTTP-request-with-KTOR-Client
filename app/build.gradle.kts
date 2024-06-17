b5c94053a839e170223e8b3cecd2e872c22a6f4b
Aamir Ashraf <aamir.ashraf@janitri.in>
2024-06-17 18:15:33 +0530

    ktor dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,13 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+   //ktor
+    implementation ("io.ktor:ktor-client-core:1.6.3")
+    implementation ("io.ktor:ktor-client-android:1.6.3")
+    implementation ("io.ktor:ktor-client-serialization:1.6.3")
+    implementation ("io.ktor:ktor-client-logging:1.6.3")
+    implementation ("ch.qos.logback:logback-classic:1.2.3")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 }
